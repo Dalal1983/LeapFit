baac2cf9c857eb56ab2f48a5e712b6f9611fc720
Sricharan <sricharanized@gmail.com>
2019-09-29 01:21:09 +0200

    added demo-seq for coach

--- UI/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    load3DModel('https://googlewebcomponents.github.io/model-viewer/examples/assets/RobotExpressive.glb');
-});
-
-function load3DModel (newModelURL, modelSide = 'left') {
-    $('#' + modelSide + 'Section').attr('src', newModelURL);   
-}
-
