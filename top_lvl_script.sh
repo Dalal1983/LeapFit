7421e7b4956e05b71c0390e2a6bb539e4fcc4a4c
Christabella Irwanto <christabella.irwanto@gmail.com>
2019-09-29 08:37:57 +0200

    Add echos and modify img_path in top_level_script

--- top_lvl_script.sh
+++ top_lvl_script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "git pulling"
 git pull
 git rm -r mixedrealitymodels/*
-python hmr/demo.py --img_path demo-sequences --glb_path mixedrealitymodels
+echo "removed mixedrealitymodels"
+python hmr/demo.py --img_path hmr/input/ --glb_path mixedrealitymodels
+echo "ran hmr"
 git add mixedrealitymodels/*
 git rm -r demo-sequences/you/*.png
+echo "pushing to website"
 git push
 
