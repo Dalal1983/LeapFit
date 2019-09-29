6700389237cc374f61e1d50e8424b6874801cbe7
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2019-09-29 10:16:22 +0200

    F

--- modules/app.js
+++ modules/app.js
@@ -1,10 +1,10 @@
 // $(document).ready(function () {
+let modelNo = 0;
 setInterval(function () {
-    let modelNo = 0;
     load3DModel('./mixedrealitymodels/' + modelNo + '.glb', 'left');
     load3DModel('./mixedrealitymodelscoach/' + modelNo + '.glb', 'right');
-    modelNo = (modelNo == 10) ? 0 : modelNo++;
-}, 3000);
+    modelNo = (modelNo == 10) ? 0 : modelNo + 1;
+}, 1000);
 
 // load3DModel('https://googlewebcomponents.github.io/model-viewer/examples/assets/RobotExpressive.glb');
 // });
