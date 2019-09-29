ea6fc77b3aa5256ae0a0abcb724878e0615519b3
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2019-09-29 08:11:11 +0200

    Reload 3D Models

--- modules/app.js
+++ modules/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
-    $('.appTitle').html(navigator.appName);
+    debugger;
+    setTimeout(function(){ load3DModel('https://googlewebcomponents.github.io/model-viewer/examples/assets/RobotExpressive.glb', 'right') }, 3000);
+    setTimeout(function(){ load3DModel('./3dmodels/Astronaut.glb', 'left') }, 3000);
 
-    
-     // load3DModel('https://googlewebcomponents.github.io/model-viewer/examples/assets/RobotExpressive.glb');
+    // load3DModel('https://googlewebcomponents.github.io/model-viewer/examples/assets/RobotExpressive.glb');
 });
 
 function load3DModel(newModelURL, modelSide = 'left') {
